b1ea5ffdb0413ef52d4a6ca878a98efdcf611dd2
BlackSmithSouL <kowalskimarcinkontakt@gmail.com>
2023-01-07 20:12:47 +0100

    Install styled components, absolute paths and create footer and header components

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+import Logo from './languages.png'
+
+export const Images = {
+    Logo
+}
---
+++ src/lib/config/config.ts
@@ -0,0 +1,6 @@
+export const APP_CONFIG = {
+    GITHUB_URL: 'https//github.com',
+    DISCORD_URL: '',
+    FLAT_ICON_URL: 'https://www.flaticon.com/free-icon/languages_3898150?term=translator&page=1&position=12&origin=search&related_id=3898150',
+    LIBRE_TRANSLATE_URL: 'https://libretranslate.com'
+}
